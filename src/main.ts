d28d9c312ca3f2e4c4427f1c8fda9e9c7b356106
Khalil <atamurotovkhalil@gmail.com>
2025-02-25 13:43:43 +0900

    latest

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://dreamy-rabanadas-1ac7fc.netlify.app/'], // ✅ Use an array
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://dreamy-rabanadas-1ac7fc.netlify.app'
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true, // Enable cookies/auth headers if needed
+    credentials: true,
   });
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
